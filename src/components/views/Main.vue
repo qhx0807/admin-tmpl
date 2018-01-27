894968830c45cb6b1968c543abc5e201b8eff781
qhx0807 <820240134@qq.com>
2018-01-27 23:41:12 +0800

    night

--- src/components/views/Main.vue
+++ src/components/views/Main.vue
@@ -72,7 +72,26 @@
           </div>
         </nav>
       </header>
-      123
+      <el-popover
+  ref="popover1"
+  placement="top-start"
+  title="标题"
+  width="200"
+  trigger="hover"
+  content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+</el-popover>
+
+<el-popover
+  ref="popover2"
+  placement="bottom"
+  title="标题"
+  width="200"
+  trigger="click"
+  content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+</el-popover>
+
+<el-button v-popover:popover1>hover 激活</el-button>
+<el-button v-popover:popover2>click 激活</el-button>
     </div>
   </div>
 </template>
