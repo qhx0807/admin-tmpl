cf9991eaea532c0cede8f5469a5fb45d0275dcee
unknown <>
2018-02-01 15:53:35 +0800

    首页报表

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,26 +1,231 @@
 <template>
   <div class="dashboard">
+    <!-- <div>
+      <p class="title">重庆第二师范学院</p>
+    </div> -->
     <el-row :gutter="10">
       <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6">
-        <el-card>
-          <i class="iconfont icon-money-o"></i>
+        <el-card :body-style="cardStyle">
+          <div class="leftbox" style="background: #00C0EF;">
+            <i class="iconfont icon-money-o moneys" style="color:#FFF;"></i>
+          </div>
+          <div class="rightbox">
+            <p>123456万元</p>
+            <p>本年第一次申报数</p>
+          </div>
         </el-card>
       </el-col>
-      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6"><el-card></el-card></el-col>
-      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6"><el-card></el-card></el-col>
-      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6"><el-card></el-card></el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6">
+        <el-card :body-style="cardStyle">
+          <div class="leftbox" style="background: #DD4B39;">
+            <i class="iconfont icon-money-o moneys" style="color:#FFF;"></i>
+          </div>
+          <div class="rightbox">
+            <p>123456万元</p>
+            <p>本年第一次申报数</p>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6">
+        <el-card :body-style="cardStyle">
+          <div class="leftbox" style="background: #00A65A;">
+            <i class="iconfont icon-money-o moneys" style="color:#FFF;"></i>
+          </div>
+          <div class="rightbox">
+            <p>123456万元</p>
+            <p>本年第一次申报数</p>
+          </div>
+        </el-card>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="6" :lg="6" :xl="6">
+        <el-card :body-style="cardStyle">
+          <div class="leftbox" style="background: #F39C12;">
+            <i class="iconfont icon-money-o moneys" style="color:#FFF;"></i>
+          </div>
+          <div class="rightbox">
+            <p>123456万元</p>
+            <p>本年第一次申报数</p>
+          </div>
+        </el-card>
+      </el-col>
     </el-row>
+    <div class="block dates">
+        <el-date-picker
+          v-model="value1"
+          type="year"
+          placeholder="选择日期" @change="time">
+        </el-date-picker>
+      </div>
+    <div class="tabOne" id="tabOne">
+
+    </div>
+    <!-- <el-row :gutter="10">
+      <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+        <div class="tabTwo" id="tabTwo"></div>
+      </el-col>
+      <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12">
+        <div class="tabThree" id="tabThree"></div>
+      </el-col>
+    </el-row> -->
+
   </div>
 </template>
 
 <script>
+import postApi from "../../axios"
+import echarts from "echarts"
 export default {
-  name: 'dashboard'
+  name: "dashboard",
+  data() {
+    return {
+      cardStyle: {
+        padding: "0px"
+      },
+      value1:new Date()
+    }
+  },
+  //一开始执行
+  created() {
+    this.tabOne(this.value1.getFullYear())
+    // this.tabTwo()
+  },
+  //渲染完成执行
+  mounted() {},
+  methods: {
+    tabOne(e) {
+      let option = { kjnd: e }
+      postApi(
+        "ESYysgl_BMhzb",
+        option,
+        response => {
+          if (response.data.Status == 1) {
+            let obj = JSON.parse(response.data.Data)
+            // console.log(JSON.stringify(obj))
+            let barList = []
+            let bmlist = []
+            let snysOne = []
+            let snzxOne = []
+            let bnsbOne = []
+            let bnsb2One = []
+            let bnjysOne = []
+            let bnysOne = []
+
+            let xbar = [
+              "上年预算", //snys
+              "上年执行数" //snzx
+            ]
+            for (let i = 0; i < obj.length; i++) {
+              bmlist.push(obj[i].bmmc)
+              snysOne.push(obj[i].snys)
+              snzxOne.push(obj[i].snzx)
+            }
+            let arr = [snysOne, snzxOne]
+
+            for (let e = 0; e < xbar.length; e++) {
+              let sdatas = {}
+              sdatas.name = xbar[e]
+              sdatas.type = "bar"
+              sdatas.data = arr[e]
+              barList.push(sdatas)
+            }
+            let option = {
+              title: {
+                text: "重庆第二师范学院"+ e +"年预算项目明细表",
+                top: "0",
+                left: "37%"
+              },
+              tooltip: {
+                trigger: "axis"
+              },
+              legend: {
+                bottom: "0px",
+                data: xbar
+              },
+              toolbox: {
+                show: true,
+                feature: {
+                  dataView: { show: true, readOnly: false },
+                  magicType: { show: true, type: ["line", "bar"] },
+                  restore: { show: true },
+                  saveAsImage: { show: true }
+                }
+              },
+              calculable: true,
+              xAxis: [
+                {
+                  type: "category",
+                  data: bmlist,
+                  axisLabel: {
+                    interval: 0, //横轴信息全部显示
+                    rotate: 25 //-30度角倾斜显示
+                  }
+                }
+              ],
+              yAxis: [
+                {
+                  type: "value"
+                }
+              ],
+              series: barList
+            }
+            var myChart = echarts.init(document.getElementById("tabOne"))
+            myChart.setOption(option)
+          } else {
+            this.$message(response.data.Message)
+          }
+        },
+        error => {
+          console.log(error)
+        }
+      )
+    },
+    time(e){
+      // console.log(e)
+      this.tabOne(e.getFullYear())
+    }
+
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.title {
+  text-align: center;
+  color: #444;
+}
+.leftbox {
+  float: left;
+  height: 50px;
+  width: 50px;
+  line-height: 50px;
+  text-align: center;
+  .moneys {
+    font-size: 40px;
+    margin: 0;
+  }
+}
+.rightbox {
+  float: right;
+  height: 50px;
+  line-height: 22px;
+  padding-right: 8px;
+  padding-top: 3px;
+  p {
+    font-size: 14px;
+    margin: 0;
+    text-align: right;
+    color: #666;
+  }
+}
+.dates{
+  text-align: center;
+  margin-top: 20px;
+}
+.tabOne {
+  height: 420px;
+  width: 100%;
+  padding-top: 24px;
+}
 </style>
 
 
