97c9fbd20518c93c93cf686e3f01c55a57dce7b6
qhx0807 <820240134@qq.com>
2018-03-21 17:20:33 +0800

    chart

---
+++ src/components/charts/Chart2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="main" style="width: 900px; height:400px;" ></div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  name: 'chart',
+  data() {
+    return {}
+  },
+  mounted() {
+    let dom = document.getElementById('main')
+    var myChart = echarts.init(dom)
+    var option = {
+      title: {
+        text: 'ECharts示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    }
+    myChart.setOption(option)
+    // this.$nextTick(function(){
+    //   alert(window.getComputedStyle(dom, null).width)
+    // })
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
