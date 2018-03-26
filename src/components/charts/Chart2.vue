126fa0766278cec0fe81cc97c2fa46047756c14c
qhx0807 <820240134@qq.com>
2018-03-26 09:54:45 +0800

    I'm down

--- src/components/charts/Chart2.vue
+++ src/components/charts/Chart2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="main" style="width: 900px; height:400px;" ></div>
+  <div id="main" style="width: 1400px; height:400px;" ></div>
 </template>
 
 <script>
@@ -10,36 +10,9 @@ export default {
     return {}
   },
   mounted() {
-    let dom = document.getElementById('main')
-    var myChart = echarts.init(dom)
-    var option = {
-      title: {
-        text: 'ECharts示例'
-      },
-      tooltip: {},
-      legend: {
-        data: ['销量']
-      },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [
-        {
-          name: '销量',
-          type: 'bar',
-          data: [5, 20, 36, 10, 10, 20]
-        }
-      ]
-    }
-    myChart.setOption(option)
-    // this.$nextTick(function(){
-    //   alert(window.getComputedStyle(dom, null).width)
-    // })
   },
 
 }
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
